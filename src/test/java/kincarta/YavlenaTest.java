14940a2efe8a4e2beebc9bb9e9e6fd7ea65adf06
Valentin Nikolov <valentin.nikolov@lab08.com>
2024-02-24 13:37:28 +0200

    test: split tests to skip havy load and multiple exceptions

--- src/test/java/kincarta/YavlenaTest.java
+++ src/test/java/kincarta/YavlenaTest.java
@@ -16,6 +16,7 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class YavlenaTest {
@@ -53,7 +54,9 @@ public class YavlenaTest {
         fluentWait = new FluentWait<>(driver);
         fluentWait.withTimeout(Duration.ofSeconds(15));
         fluentWait.pollingEvery(Duration.ofMillis(250));
-        fluentWait.ignoring(StaleElementReferenceException.class, ElementClickInterceptedException.class);
+        fluentWait.ignoring(StaleElementReferenceException.class);
+        fluentWait.ignoring(NoSuchElementException.class);
+        fluentWait.ignoring(ElementClickInterceptedException.class);
 
         driver.manage().window().maximize();
 
@@ -67,7 +70,7 @@ public class YavlenaTest {
     }
 
     @Test
-    public void testYavlenaSite() {
+    public void testYavlenaSiteNamesAdressesAndNumberOfProperties() {
         driver.get("https://www.yavlena.com/broker/");
 
         WebElement hideCookiesBtn = driver.findElement(By.className("hide-cookies-message"));
@@ -85,19 +88,16 @@ public class YavlenaTest {
 
         ArrayList<WebElement> articlesList = (ArrayList<WebElement>) driver.findElements(By.tagName("article"));
         ArrayList<Broker> brokersList = new ArrayList<>();
+        final String[] brokerNames = new String[1];
 
         articlesList.forEach(webElement -> {
             Broker broker = new Broker();
-            broker.setName(webElement.findElement(By.cssSelector(".name a")).getAttribute("title"));
+            brokerNames[0] = webElement.findElement(By.cssSelector(".name a")).getAttribute("title");
+            broker.setName(brokerNames[0]);
             broker.setAddress(webElement.findElement(By.className("office")).getText());
-            broker.setLandlinePhone(webElement.findElement(
-                    By.xpath("(//div[@class='tel-group']//a)[1]")).getText());
-            broker.setMobilePhone(webElement.findElement(
-                    By.xpath("(//div[@class='tel-group']//a)[2]")).getText());
             broker.setNumProperties(webElement.findElement(By.cssSelector(".position a[title]")).getText());
             brokersList.add(broker);
         });
-
 
         ArrayList<Broker> brokersListOuter = new ArrayList<>(brokersList);
         final WebElement[] searchInput = new WebElement[1];
@@ -133,10 +133,6 @@ public class YavlenaTest {
             ArrayList<WebElement> numberOfArticlesSameName =
                     (ArrayList<WebElement>) driver.findElements(
                             By.xpath("//article//h3/a[@title='" + brokerName + "']"));
-
-            ArrayList<WebElement> actualLandlinePhoneList = new ArrayList<>();
-            ArrayList<WebElement> actualMobilePhoneList = new ArrayList<>();
-
 
             brokersList.forEach(broker -> {
                 if(brokerName.equals(broker.getName())) {
@@ -192,6 +188,106 @@ public class YavlenaTest {
             ArrayList<WebElement> numberOfArticles =
                     (ArrayList<WebElement>) driver.findElements(By.xpath("//article//h3/a[@title]"));
             Assert.assertTrue(numberOfArticles.size() == numberOfArticlesSameName.size());
+
+            WebElement clearBtn = driver.findElement(By.className("clear-btn"));
+
+            fluentWait.until(ExpectedConditions.elementToBeClickable(clearBtn));
+            clearBtn.click();
+
+            WebElement brokersLoading = driver.findElement(By.cssSelector(".brokers-loading[style='display: block;']"));
+            fluentWait.until((ExpectedConditions.invisibilityOf(brokersLoading)));
+        });
+    }
+
+
+    @Test
+    public void testYavlenaSiteLandlinePhones() {
+        driver.get("https://www.yavlena.com/broker/");
+
+        WebElement hideCookiesBtn = driver.findElement(By.className("hide-cookies-message"));
+        WebElement loadMoreBtn = driver.findElement(By.className("load-more-results-list"));
+
+        fluentWait.until(ExpectedConditions.elementToBeClickable(hideCookiesBtn));
+        hideCookiesBtn.click();
+
+        fluentWait.until(ExpectedConditions.elementToBeClickable(loadMoreBtn));
+        js.executeScript("arguments[0].click();", loadMoreBtn);
+
+        fluentWait.until(ExpectedConditions.invisibilityOf(loadMoreBtn));
+
+        ArrayList<WebElement> articlesList = (ArrayList<WebElement>) driver.findElements(By.tagName("article"));
+        ArrayList<Broker> brokersList = new ArrayList<>();
+        final String[] brokerNames = new String[1];
+        final String[] mobilePhones = new String[1];
+
+        articlesList.forEach(webElement -> {
+            Broker broker = new Broker();
+            brokerNames[0] = webElement.findElement(By.cssSelector(".name a")).getAttribute("title");
+            broker.setName(brokerNames[0]);
+            broker.setAddress(webElement.findElement(By.className("office")).getText());
+            broker.setLandlinePhone(webElement.findElement(
+                    By.xpath("(//article//a[@title='" + brokerNames[0] + "']/parent::h3/parent::div/" +
+                            "following-sibling::div[@class='tel-group']/span[@class='tel']/a)[1]")).getText());
+//            try {
+//                mobilePhones[0] = Arrays.toString(new String[]{webElement.findElement(
+//                        By.xpath("(//article//a[@title='" + brokerNames[0] + "']/parent::h3/parent::div/" +
+//                                "following-sibling::div[@class='tel-group']/span[@class='tel']/a)[2]")).getText()});
+//            } catch (org.openqa.selenium.NoSuchElementException noSuchElementException) {
+//                mobilePhones[0] = "";
+//            }
+            //     broker.setMobilePhone(mobilePhones[0]);
+            broker.setNumProperties(webElement.findElement(By.cssSelector(".position a[title]")).getText());
+            brokersList.add(broker);
+        });
+
+        ArrayList<Broker> brokersListOuter = new ArrayList<>(brokersList);
+        final WebElement[] searchInput = new WebElement[1];
+
+        brokersListOuter.forEach(brokerOuter -> {
+            searchInput[0] = (driver.findElement(By.className("input-search")));
+
+            fluentWait.until(ExpectedConditions.visibilityOf(searchInput[0]));
+
+            String brokerName = brokerOuter.getName();
+            searchInput[0].sendKeys(brokerName);
+
+            AtomicReference<WebElement> actualName =
+                    new AtomicReference<>(driver.findElement(
+                            By.xpath("(//a[@title='" + brokerName + "'])[1]")));
+            fluentWait.until(ExpectedConditions.visibilityOf(actualName.get()));
+
+            AtomicReference<ArrayList<WebElement>> actualLandlinePhoneList =
+                    new AtomicReference<>((ArrayList<WebElement>) driver.findElements(
+                            By.xpath("(//article[1]//a[@title='" + brokerName + "']/parent::h3/" +
+                                    "parent::div/following-sibling::div[@class='tel-group']/span[@class='tel']/a)[1]")));
+            actualLandlinePhoneList.get().forEach(actualLandlinePhone -> {
+                fluentWait.until(ExpectedConditions.visibilityOf(actualLandlinePhone));
+            });
+
+            brokersList.forEach(broker -> {
+                if(brokerName.equals(broker.getName())) {
+
+                    final boolean[] flagForLandlinePhoneMatch = new boolean[1];
+                    actualLandlinePhoneList.get().forEach(actualLandlinePhone -> {
+                        try {
+                            if (actualLandlinePhone.getText().equals(broker.getLandlinePhone())) {
+                                flagForLandlinePhoneMatch[0] = true;
+                            }
+                        } catch (StaleElementReferenceException sere) {
+                            actualLandlinePhoneList.set((ArrayList<WebElement>) driver.findElements(
+                                    By.xpath("(//article[1]//a[@title='" + brokerName + "']/parent::h3/" +
+                                            "parent::div/following-sibling::div[@class='tel-group']/span[@class='tel']/a)[1]")));
+                            actualLandlinePhoneList.get().forEach(actualLandlinePhoneRetry -> {
+                                fluentWait.until(ExpectedConditions.visibilityOf(actualLandlinePhoneRetry));
+                                if (actualLandlinePhoneRetry.getText().equals(broker.getLandlinePhone())) {
+                                    flagForLandlinePhoneMatch[0] = true;
+                                }
+                            });
+                        }
+                    });
+                    Assert.assertTrue(flagForLandlinePhoneMatch[0]);
+                }
+            });
 
             WebElement clearBtn = driver.findElement(By.className("clear-btn"));
 
